4b181d3044fd9d704a15a01f2f9c8004b707dd2c
jiho-k0409 <sjihoupreme@gmail.com>
2021-12-06 21:57:22 +0900

    퀘리셀렉터오류해결필요

---
+++ public/main.js
@@ -0,0 +1,104 @@
+var now = new Date();
+var month = now.getMonth()+1;
+const prevMonth = document.querySelector('.prev');
+const nextMonth = document.querySelector('.next');
+const titleMonth= document.querySelector('.month');
+const dateContainer = document.querySelector('.dateContainer')
+console.log(dateContainer)
+const dateButton = dateContainer.querySelectorAll('.date')
+
+titleMonth.innerHTML = month;
+
+prevMonth.addEventListener('click', function(event){
+    month -= 1;
+    titleMonth.innerHTML = month;
+    if (month<1){
+        month=12;
+        titleMonth.innerHTML = month;
+    };
+    makeDate()
+});
+
+nextMonth.addEventListener('click', function(event){
+    month +=1;
+    titleMonth.innerHTML = month;
+    if (month>12) {
+        month=1
+        titleMonth.innerHTML = month;
+    };
+    makeDate()
+});
+
+
+for(let i = 0 ; i<dateButton.length;){
+    dateButton.addEventListener('click',(event)=>{
+    console.log('clicked!!')
+    })
+}
+
+
+
+function thirtyOne(){
+    revealDate30();
+    revealDate31();
+}
+function thirty(){
+    revealDate30();
+    concealDate31();
+}
+function twemtyNine(){
+    concealDate31();
+    concealDate30();
+}
+
+function makeDate(){
+    if(month==1){
+        thirtyOne();
+    };
+    if(month==2){
+        twemtyNine()
+    };
+    if(month==3){
+        thirtyOne();
+    };
+    if(month==4){
+        thirty();
+    };
+    if(month==5){
+        thirtyOne();
+    };
+    if(month==6){
+        thirty();
+    };
+    if(month==7){
+        thirtyOne();
+    };
+    if(month==8){
+        thirtyOne();
+    };
+    if(month==9){
+        thirty();
+    };
+    if(month==10){
+        thirtyOne();
+    };
+    if(month==11){
+        thirty();
+    };
+    if(month==12){
+        thirtyOne();
+    };
+};
+
+function concealDate30(){
+    document.querySelector('#threeO').style.display='none';
+};
+function concealDate31(){
+    document.querySelector('#threeOne').style.display='none';
+};
+function revealDate30(){
+    document.querySelector('#threeO').style.display='block';
+};
+function revealDate31(){
+    document.querySelector('#threeOne').style.display='block';
+};
