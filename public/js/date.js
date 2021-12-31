a27e4a267b885e00444140a541fac8336f4c5d45
jiho-k0409 <sjihoupreme@gmail.com>
2021-12-31 23:39:15 +0900

    .

--- public/js/date.js
+++ public/js/date.js
@@ -8,5 +8,3 @@ const Month = document.querySelector('#todayMonth')
 Month.innerHTML = month
 const Day = document.querySelector('#todayDate')
 Day.innerHTML = day
-
-alert('오른쪽 아래 버튼을 누르시면 일정을 만들 수 있습니다. 다른 사람이 만든 일정을 볼 수 있습니다. 이상한 점 있으면 디엠 부탁드립니다. 이 페이지 제목 좋은거 있으시면 추천해주시면 감사하겠습니다.')
